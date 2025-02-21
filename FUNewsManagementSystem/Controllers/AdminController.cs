f615ebe8d1aab54f1d51cb2e21b66f1fa62ddfb4
Dev-FStacker <bennguyen.contact@gmail.com>
2025-02-22 04:36:20 +0700

    feat: crud user

--- FUNewsManagementSystem/Controllers/AdminController.cs
+++ FUNewsManagementSystem/Controllers/AdminController.cs
@@ -1,14 +1,63 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessObject.Service;
+using DataAccessObject.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace FUNewsManagementSystem.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly IAccountService _accountService;
+
+        public AdminController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
         public IActionResult Dashboard()
         {
             return View();
+        }
+
+        public async Task<IActionResult> ManageUsers()
+        {
+            var users = await _accountService.GetSystemAccountsAsync();
+            return View("_ManageUsers", users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] SystemAccount account)
+        {
+            if (account == null)
+                return BadRequest("Invalid account data.");
+
+            var result = await _accountService.CreateSystemAccountAsync(account);
+            if (!result)
+                return BadRequest("Failed to create user.");
+            return Ok("User created successfully.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser(short id)
+        {
+            var result = await _accountService.DeleteSystemAccountAsync(id);
+            if (!result)
+                return BadRequest("Failed to delete user.");
+
+            return Ok("User deleted successfully.");
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser([FromBody] SystemAccount account)
+        {
+            if (account == null)
+                return BadRequest("Invalid account data.");
+
+            await _accountService.UpdateSystemAccountAsync(account);
+            return Ok("User updated successfully.");
         }
     }
 }
